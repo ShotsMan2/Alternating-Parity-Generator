c982934ed4618e2f7fb0681390b642504847a9c5
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:10:48 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,37 +4,40 @@
 
 int main()
 {
-    int i,sayi,n,sum;
+    int i, sayi, n, sum;
 
     printf("n i giriniz");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    sum=2;
+    sum = 2;
 
     srand(time(NULL));
 
-    for(i=0;i<n;i++){
-        for(;;){
-            sayi=rand()%100+1;
+    for (i = 0; i < n; i++)
+    {
+        for (;;)
+        {
+            sayi = rand() % 100 + 1;
 
-            if(sum%2==0){
-                if(sayi%2!=0){
-                printf("%d ",sayi);
-                sum++;
-                break;
-                }
-            }
-
-            if(sum%2!=0){
-                if(sayi%2==0){
-                    printf("%d ",sayi);
+            if (sum % 2 == 0)
+            {
+                if (sayi % 2 != 0)
+                {
+                    printf("%d ", sayi);
                     sum++;
                     break;
-
                 }
-
             }
 
+            if (sum % 2 != 0)
+            {
+                if (sayi % 2 == 0)
+                {
+                    printf("%d ", sayi);
+                    sum++;
+                    break;
+                }
+            }
         }
     }
 }
